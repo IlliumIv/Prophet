abd7933c57bf4971ba3f2004fa8a42649d6e446d
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2021-05-24 12:02:13 -0700

    Update Prophet.cs

--- Prophet.cs
+++ Prophet.cs
@@ -27,7 +27,7 @@ namespace Prophet
             _windowOffset = GameController.Window.GetWindowRectangle().TopLeft;
 
             RefreshData();
-            Settings.Enable.OnValueChanged += (s,v) => { RefreshData(); };
+            Settings.Enable.OnValueChanged += (s,v) => { Settings.Enable.Value = v; RefreshData(); };
             Settings.Refresh.OnPressed += () => { RefreshData(); };
             Settings.League.OnValueSelectedPre += s => { RefreshData(); };
             return true;
